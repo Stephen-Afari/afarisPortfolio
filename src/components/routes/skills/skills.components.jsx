be2539ec687344ac110f34d2d22d4820b3a3da92
Stephen Afari <demorganafari@yahoo.com>
2024-05-13 09:40:15 +0000

    skills page

--- src/components/routes/skills/skills.components.jsx
+++ src/components/routes/skills/skills.components.jsx
@@ -1,5 +1,36 @@
+import './skills.styles.scss'
 export const Skills=()=>{
     return(
-        <h1>Skills!!</h1>
+        <div className='skillsContainer'>
+   <div>
+    <p className='skillText'> <span className='skillHeader'>Front End:</span> Javascript ,React/Redux (routing, context API, Sass, Styled components),TypeScript, Pug ,HTML, CSS, GraphQL (Apollo), Progressive Web Apps, Data Structures & Algorithm</p>
+    <br/>
+  <p className='skillText'><span className='skillHeader'>BackEnd:</span> Restful API design, NodeJS ,Mongodb/Mongoose,data Modelling, aggregation pipeline ,Authentication with JWT ,MVC Architecture ,Express ,Mail trap ,Security, Error handling ,Geospatial queries, Stripe ,Postgre SQL, Firebase/Firestore</p>
+  <br/>
+  <br/>
+  <br/>
+  
+   </div>
+            <p className="skillIcons">
+<a className='skillsets' href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg" width="36" height="36" alt="JavaScript" /></a>
+<a className='skillsets' href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg" width="36" height="36" alt="TypeScript" /></a>
+<a className='skillsets' href="https://git-scm.com/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/git-colored.svg" width="36" height="36" alt="Git" /></a>
+<a className='skillsets' href="https://reactjs.org/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg" width="36" height="36" alt="React" /></a>
+<a className='skillsets' href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg" width="36" height="36" alt="HTML5" /></a>
+<a className='skillsets' href="https://jquery.com/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/jquery-colored.svg" width="36" height="36" alt="JQuery" /></a>
+<a className='skillsets' href="https://sass-lang.com/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/sass-colored.svg" width="36" height="36" alt="Sass" /></a>
+<a className='skillsets' href="https://getbootstrap.com/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg" width="36" height="36" alt="Bootstrap" /></a>
+<a className='skillsets' href="https://redux.js.org/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/redux-colored.svg" width="36" height="36" alt="Redux" /></a>
+<a className='skillsets' href="https://webpack.js.org/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/webpack-colored.svg" width="36" height="36" alt="Webpack" /></a>
+<a className='skillsets' href="https://babeljs.io/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/babel-colored.svg" width="36" height="36" alt="Babel" /></a>
+<a className='skillsets' href="https://www.w3.org/TR/CSS/#css" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg" width="36" height="36" alt="CSS3" /></a>
+<a className='skillsets' href="https://nodejs.org/en/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg" width="36" height="36" alt="NodeJS" /></a>
+<a className='skillsets' href="https://expressjs.com/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg" width="36" height="36" alt="Express" /></a>
+<a className='skillsets' href="https://graphql.org/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/graphql-colored.svg" width="36" height="36" alt="GraphQL" /></a>
+<a className='skillsets' href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg" width="36" height="36" alt="MongoDB" /></a>
+<a className='skillsets' href="https://firebase.google.com/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/firebase-colored.svg" width="36" height="36" alt="Firebase" /></a>
+<a className='skillsets' href="https://www.heroku.com/" target="_blank" rel="noreferrer"><img  src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/heroku-colored.svg" width="36" height="36" alt="Heroku" /></a>
+</p>
+        </div>
     )
 }
